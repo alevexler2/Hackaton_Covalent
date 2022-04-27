2345af8971a27b0151a6ff0fd2d13b19ea0bf07e
alevexler <alevexler2@gmail.com>
2022-04-26 21:02:49 -0300

    initial Commit

---
+++ scripts/app.js
@@ -0,0 +1,24 @@
+const inputUser = document.querySelector("#inputUser");
+const btnUser = document.querySelector("#btnUser");
+const container = document.querySelector("#container");
+btnUser.addEventListener("click",()=>{
+  let address = inputUser.value;
+  let url = `https://api.covalenthq.com/v1/1/address/${address}/balances_v2/?key=ckey_8129ee43a92e4bec8574fc26e05`;
+    fetch(url)
+      .then(response=>response.json())
+      .then(data=>{data.data.items.forEach(e => {
+        console.log(e);
+        container.innerHTML = `<div class="card" style="width: 18rem;">
+        <img src="${e.logo_url}" class="card-img-top" alt="">
+        <div class="card-body">
+          <h5 class="card-title">Address: ${address}</h5>
+          <p class="card-text">Balance: ${e.contract_name} </p>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Quote: ${e.quote}</li>
+          <li class="list-group-item">Quote: ${e.quote_rate}</li>
+        </ul>
+        </div>`;
+      });
+      });
+})
